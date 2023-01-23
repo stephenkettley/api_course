f2faa70aaecfa1da2f5ac301b71259d1da782ed7
Stephen Kettley <kettleystephen@gmail.com>
2023-01-23 11:20:47 +0200

    implemented http put method

--- src/main.py
+++ src/main.py
@@ -67,9 +67,27 @@ def delete_post(id: int):
     index = find_index_post(id)
 
     if index == None:
-        return HTTPException(
+        raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id {id} doest not exist",
         )
+
     my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id {id} doest not exist",
+        )
+
+    post_dict = post.dict()
+    post_dict["id"] = id
+    my_posts[index] = post_dict
+    return {"new_data": post_dict}
